b62294255ea99c947b5d3dc0e0bc98f55300a231
MemoryController <2846745554@qq.com>
2025-11-04 22:19:08 +0800

    lesson have some questions with io

---
+++ 2025-11-04/排列计数.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int num,prefix[(int)2e5+10];
+long long ans,cnt;
+const long long MOD = 998244353;
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int a;
+    cin>>a;
+    while (a--){
+        ans = 1;
+        cin>>num;
+        bool ok = true;
+        for(int i=1; i<=num; i++){
+            cin>>prefix[i];
+            if(i>=2 && prefix[i]>prefix[i-1]){
+                ok =0;
+            }
+        }
+        if(prefix[num] != 1 || prefix[1]>num){
+            // 不然就错了
+            ok = 0;
+        }
+        if(ok){
+            cnt = num - prefix[1]; // 剩多少数字可以选
+            for(int i=2; i<=num; i++){
+                if(prefix[i] == prefix[i-1]){
+                    ans = ans * cnt % MOD; // 遍历过去就相当于求排列数了
+                    cnt--;
+                }else{
+                    cnt = cnt + prefix[i-1] - prefix[i] -1;
+                }
+
+            }
+            cout<<ans<<'\n';
+
+
+        }else{
+            cout<<"0\n";
+        }
+    }
+    
+
+
+    return 0;
+}
