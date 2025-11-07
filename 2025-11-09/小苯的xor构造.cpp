a2da94861711a12833664835ff2ab472720e12c5
MemoryController <2846745554@qq.com>
2025-11-07 15:46:27 +0800

    prelearn

---
+++ 2025-11-09/小苯的xor构造.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+    int k;
+    cin>>k;
+    // 0和任何数取异或都是那个数
+    cout<<k<<" 0"<<'\n';
+
+
+    return 0;
+}
