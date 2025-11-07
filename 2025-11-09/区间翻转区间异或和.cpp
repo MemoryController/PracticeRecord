14d74f835f3f1f64d6294deb82f5f79644bfacbf
MemoryController <2846745554@qq.com>
2025-11-07 15:40:28 +0800

    prelearn

---
+++ 2025-11-09/区间翻转区间异或和.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+const int N = 4e5+10;
+
+ll arr[N],pre_max=-INF,vis[(int)2e6+10];
+ll num,ans=0;
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+    cin>>num;
+    for(ll i=1; i<=num; i++){
+        cin>>arr[i];
+    }
+    //求异或和
+    for(ll i=1; i<=num; i++){
+        // 和0异或就是自己，不需要特殊处理
+        arr[i] ^= arr[i-1];
+    }
+    //对每个位置进行计数包括0号位置
+    for(ll i=0; i<=num; i++){
+        vis[arr[i]]++;
+        pre_max = max(arr[i],pre_max);
+    }
+    // 组合数 两个相同的前缀异或代表夹在他们中间的是一个灵异区间
+    for(ll i=0; i<=pre_max; i++){
+        ans += vis[i]*(vis[i]-1)/2;
+    }
+    cout<<ans<<'\n';
+    
+
+
+    return 0;
+}
